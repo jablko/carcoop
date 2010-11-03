eb60fecf15e564d219baf7083eb0c89441f6760b
Jack Bates <ms419@freezone.co.uk>
2010-11-03 16:55:22 -0700

    Use QUnit, test passes

--- test/index.js
+++ test/index.js
@@ -1,21 +1,18 @@
-jQuery.ajax({
+test('', function ()
+  {
+    stop();
 
-  data: { url: 'test/googleMap.html' },
-  url: '../latlng',
+    jQuery.ajax({
 
-  success: function (data)
-    {
-      jQuery(function ($)
+      data: { url: 'test/googleMap.html' },
+      url: '../latlng',
+
+      success: function (data)
         {
-          if ([[49.277339, -123.134425],
+          same(data, [[49.277339, -123.134425],
               [49.208652, -122.919473],
-              [49.267853, -123.071342]] === data)
-          {
-            $('<div class="pass">PASS</div>').appendTo('body');
-          }
-          else
-          {
-            $('<div class="fail">FAIL</div>').appendTo('body');
-          }
-        });
-    } });
+              [49.267853, -123.071342]]);
+
+          start();
+        } });
+  });
