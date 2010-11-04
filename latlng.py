0649f6381d67b8cd310cd893ae01f3e863d71437
Jack Bates <ms419@freezone.co.uk>
2010-11-04 08:52:36 -0700

    Switch to webapp framework, for access to self.request.url

--- latlng.py
+++ latlng.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import cgi, httplib, json, re, sys, urlparse
+import httplib, json, re, urlparse
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 # Create HTTP connection from URL scheme, host, and port
 def http_connection(url):
@@ -11,25 +13,26 @@ def http_connection(url):
 
   return httplib.HTTPConnection(components[1])
 
-print 'Content-Type: application/json'
-print
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'application/json'
 
-method = 'GET'
-url = 'http://www.cooperativeauto.net/locations/googleMap'
+    method = 'GET'
+    url = 'http://www.cooperativeauto.net/locations/googleMap'
 
-try:
-  url = cgi.parse()['url'][0]
+    url = self.request.get('url', url)
+    url = urlparse.urljoin(self.request.url, url)
 
-except KeyError:
-  pass
+    conn = http_connection(url)
 
-conn = http_connection(url)
+    conn.request(method, url)
+    response = conn.getresponse()
 
-conn.request(method, url)
-response = conn.getresponse()
+    obj = []
+    for match in re.finditer('GLatLng\((.+?), (.+?)\)', response.read()):
+      obj.append([match.group(1), match.group(2)])
 
-obj = []
-for match in re.finditer('GLatLng\((.+?), (.+?)\)', response.read()):
-  obj.append([match.group(1), match.group(2)])
+    json.dump(obj, self.response.out)
 
-json.dump(obj, sys.stdout)
+if __name__ == '__main__':
+  run_wsgi_app(webapp.WSGIApplication([('/latlng', MainPage)]))
