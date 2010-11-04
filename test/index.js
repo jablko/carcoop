bed612d1fddede31739e4b8b52f1831a55ebb112
Jack Bates <ms419@freezone.co.uk>
2010-11-04 14:31:24 -0700

    Add tests for <name/> and <description/>

--- test/index.js
+++ test/index.js
@@ -9,7 +9,12 @@ test('', function ()
 
       success: function (data)
         {
-          equal($('coordinates', data).text(), '-123.134425,49.277339-122.919473,49.208652-123.071342,49.267853');
+          equal($('name', data).text(),
+            'BeachNW - Ash & Fourth AvenuePaloma');
+          equal($('description', data).text(),
+            'Vancouver - Beach Avenue & Thurlow StreetNew Westminster - Ash Street & Fourth AvenueVancouver - East 3rd Avenue & Cotton Drive');
+          equal($('coordinates', data).text(),
+            '-123.134425,49.277339-122.919473,49.208652-123.071342,49.267853');
 
           start();
         } });
